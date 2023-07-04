5c568fa11f9bcfc052aaee6d9aa4f5907e16a96b
Rafael <mtadayz7@gmail.com>
2023-07-04 12:54:31 -0300

    correcao em alguns metodos

--- g_arquivo_ctk.py
+++ g_arquivo_ctk.py
@@ -6,7 +6,7 @@ import datetime as dt
 from PIL import ImageTk
 from pathlib import Path
 from hurry.filesize import alternative, size as sz
-from customtkinter import CTk, CTkEntry, CTkLabel, CTkButton, CTkFrame, CTkFont, CTkScrollbar
+from customtkinter import CTk, CTkEntry, CTkLabel, CTkButton, CTkFrame, CTkFont, CTkScrollbar, CTkToplevel
 from icons import *
 from base64 import b64decode
 
@@ -42,6 +42,7 @@ class App(CTk):
         self.style.layout('Treeview', [('Treeview.theearea', {'sticky':'nswe'})])
         self.label_font = CTkFont(family='Roboto Slab', size=18, weight='bold')
         self.entry_font = CTkFont(family='Roboto Slab', size=18)
+        self.button_font = CTkFont(family='Roboto Slab', size=15, weight='bold')
         self.style.configure('TMenu.tk_popup', background='black')
         self.style.configure('TMenubutton', foreground='black', font=('Roboto Slab', 15), background="#1f538d", padding=5, width=3)
         #Variables
@@ -49,7 +50,7 @@ class App(CTk):
         self.hidden_folder = StringVar()
         self.hidden_folder.set('off')
         self.copy_file_name = ''
-        self.current_path_copy_file = ''
+        self.current_copy_path = ''
         #Frames
         self.f_main = CTkFrame(self, corner_radius=25)
         self.f_browser = CTkFrame(self.f_main, height=50, fg_color='transparent')
@@ -59,10 +60,15 @@ class App(CTk):
         self.f_current_path = CTkFrame(self.f_search, height=20, fg_color='transparent')
         
         #menu right click
-        self.f_browser_files.menu = Menu(self.f_browser_files, tearoff=0, font=('Roboto Slab', 12), activeborderwidth=2, bd=2)
-        self.f_browser_files.menu.add_command(label='Copy', command=self.copy)
-        self.f_browser_files.menu.add_command(label='Paste', command=self.paste_file)
-        self.f_browser_files.menu.add_command(label='Delete', command=self.delete)
+        self.f_browser_files.context_menu = Menu(self.f_browser_files, tearoff=0, font=('Roboto Slab', 12), activeborderwidth=2, bd=2)
+        self.f_browser_files.context_menu.add_command(label='Copy', command=self.copy)
+        self.f_browser_files.context_menu.add_command(label='Paste', command=self.to_paste)
+        self.f_browser_files.context_menu.add_command(label='Delete', command=self.delete)
+        self.f_browser_files.context_menu.add_separator()
+        self.f_browser_files.context_menu_create = Menu(self.f_browser_files, tearoff=0, font=('Roboto Slab', 12))
+        self.f_browser_files.context_menu_create.add_command(label='Folder', command=self.window_creation_folder)
+        self.f_browser_files.context_menu_create.add_command(label='File')
+        self.f_browser_files.context_menu.add_cascade(label='Create New +', menu=self.f_browser_files.context_menu_create)
         
         #Treeview
         self.tview_files = Treeview(self.f_browser_files, columns=['#01', '#02', '#03','#04'], show='tree headings',
@@ -94,38 +100,55 @@ class App(CTk):
         self.scrollbar_y.pack(side=RIGHT, fill=Y)
         
         #Loads frame widgets
-        self.loader_info_widgets()
-        self.loader_search_widgets()
+        self.load_info_widgets()
+        self.load_search_widgets()
         
         #Loads Files and Folders
-        self.loader_files()
+        self.upload_files()
         
-    def menu(self, event):
+    def context_menu(self, event):
         try:
-            self.f_browser_files.menu.tk_popup(event.x_root, event.y_root)
+            self.f_browser_files.context_menu.tk_popup(event.x_root, event.y_root)
             row = self.tview_files.selection()
             if len(row) >=1:
                 self.copy_file_name = self.tview_files.item(row[0]).get('values')[0]
         finally:
-            self.f_browser_files.menu.grab_release()
-            self.f_browser_files.menu.bind('<FocusOut>', self.exit_menu)
+            self.f_browser_files.context_menu.grab_release()
+            self.check_delete_option()
+            self.check_paste_option()
+            self.check_creation_menu_options()
+            self.f_browser_files.context_menu.bind('<FocusOut>', self.close_context_menu)
     
-    def exit_menu(self, event):
-        self.f_browser_files.menu.unpost()
+    def close_context_menu(self, event):
+        self.f_browser_files.context_menu.unpost()
     
     def copy(self):
         if self.copy_file_name != '':
-            self.current_path_copy_file = Path(self.current_path, self.copy_file_name)
-            self.verific_paste()
+            self.current_copy_path = Path(self.current_path, self.copy_file_name)
+            self.check_paste_option()
    
-    def verific_paste(self):
-        if self.current_path_copy_file != '' and Path.exists(self.current_path_copy_file):
-            self.f_browser_files.menu.entryconfig('Paste', state='normal')
+    def check_paste_option(self):
+        if self.current_copy_path != '' and Path.exists(self.current_copy_path):
+            self.f_browser_files.context_menu.entryconfig('Paste', state='normal')
         else:
-            self.f_browser_files.menu.entryconfig('Paste', state='disabled')
+            self.f_browser_files.context_menu.entryconfig('Paste', state='disabled')
     
-    def paste_file(self):
-        if self.current_path_copy_file != '' and Path.exists(self.current_path_copy_file) and Path.is_file(self.current_path_copy_file):
+    def check_creation_menu_options(self):
+        if Path.owner(self.current_path) != str(Path.home()).split('/')[-1]:
+            self.f_browser_files.context_menu_create.entryconfig('Folder', state = 'disabled')
+            self.f_browser_files.context_menu_create.entryconfig('File', state = 'disabled')
+        else:
+            self.f_browser_files.context_menu_create.entryconfig('Folder', state = 'normal')
+            self.f_browser_files.context_menu_create.entryconfig('File', state = 'normal')
+        
+    def check_delete_option(self):
+        if Path.owner(self.current_path) == str(Path.home()).split('/')[-1] and len(self.tview_files.selection()) == 1:
+            self.f_browser_files.context_menu.entryconfig('Delete', state='normal')
+        else:
+            self.f_browser_files.context_menu.entryconfig('Delete', state='disabled')
+      
+    def to_paste(self):
+        if self.current_copy_path != '' and Path.exists(self.current_copy_path) and Path.is_file(self.current_copy_path):
             #Corrigir essa parte da copia de arquivos existentes
             if Path.exists(Path(self.current_path, self.copy_file_name)):
                 file_name = self.copy_file_name.split('.')
@@ -135,10 +158,10 @@ class App(CTk):
                     new_name = f'{file_name[0]}1.{file_name[1]}'
                 shutil.copy(Path(self.current_path, self.copy_file_name), Path(self.current_path, new_name))
             else:
-                shutil.copy(self.current_path_copy_file, self.current_path)
-        if Path.is_dir(self.current_path_copy_file):
-            shutil.copytree(self.current_path_copy_file, Path(self.current_path, self.copy_file_name), copy_function=shutil.copy)
-        self.loader_files() 
+                shutil.copy(self.current_copy_path, self.current_path)
+        if Path.is_dir(self.current_copy_path):
+            shutil.copytree(self.current_copy_path, Path(self.current_path, self.copy_file_name), copy_function=shutil.copy)
+        self.upload_files() 
         
     def delete(self):   
         file = Path(self.current_path, self.copy_file_name)
@@ -155,10 +178,37 @@ class App(CTk):
                     shutil.rmtree(file, ignore_errors=True)
                     
             self.copy_file_name = ''
-            self.current_path_copy_file = ''
-            self.loader_files()
+            self.current_copy_path = ''
+            self.upload_files()
    
-    def loader_files(self):
+    def window_creation_folder(self):
+        def create_folder():
+            Path(self.current_path, window.name_folder.get()).mkdir(exist_ok=True)
+            window.name_folder.delete(0, END)
+            self.upload_files()
+            
+        window = CTkToplevel(self)
+        window.title("New Folder - Filemanager")
+        window.geometry('500x100')
+        window.resizable(False, False)
+        
+        window.name_folder = CTkEntry(window, placeholder_text='enter the name of the folder', font=self.entry_font)
+        window.f_button = CTkFrame(window, fg_color='transparent')
+        
+        window.b_ok = CTkButton(window.f_button, text='Ok', font=self.button_font, command=create_folder)
+        window.b_cancel = CTkButton(window.f_button, text='Cancel', command=window.destroy, font=self.button_font)
+        
+        window.name_folder.pack(padx=10, fill=X, pady=15)
+        window.f_button.pack(fill=X, padx=10)
+        window.b_ok.pack(side=LEFT, padx=30, fill=X, expand=True)
+        window.b_cancel.pack(side=RIGHT, padx=25, fill=X, expand=True)
+
+        window.focus_force()
+        window.grab_set()
+        window.bind('WM_DELETE_WINDOW', window.destroy)
+        window.mainloop()
+            
+    def upload_files(self):
         #Loads Files and Folders from current Path
         files = sorted(os.listdir(self.current_path), reverse=True)
         if self.hidden_folder.get() == 'off':
@@ -178,13 +228,12 @@ class App(CTk):
                 self.tview_files.insert('',END, values=(file, date_modified, extension_file, size), image=self.icons[extension_file])
             else: 
                 self.tview_files.insert('',END, values=(file, date_modified, extension_file, size))
-        self.loader_current_path()
-        self.tview_files.bind('<Double-1>', self.loader_next_folder)
-        self.tview_files.bind('<Button-3>', self.menu)
-        self.tview_files.bind('<Return>', self.loader_next_folder)
-        self.verific_paste()
+        self.load_current_path()
+        self.tview_files.bind('<Double-1>', self.load_next_files)
+        self.tview_files.bind('<Button-3>', self.context_menu)
+        self.tview_files.bind('<Return>', self.load_next_files)
     
-    def loader_next_folder(self, event):
+    def load_next_files(self, event):
         #Loader files and folders of next folder    
         folder = self.tview_files.selection()
         if len(folder) == 1:
@@ -193,48 +242,49 @@ class App(CTk):
                 self.current_path = Path(self.current_path, folder)
             if folder == '...':
                 self.current_path = self.current_path.parent
-            self.loader_files()
+            self.upload_files()
     
-    def loader_info_widgets(self):
+    def load_info_widgets(self):
         #Loads disks and sourcer dir
         if list(os.uname())[0] == 'Linux':
             user = list(os.popen('whoami'))[0].rsplit('\n')[0]
             source = Path('/')
             discs = os.listdir(f'/media/{user}')
-            CTkButton(self.f_info, text=source, command= lambda : self.loader_file_disc(source)).pack(side=LEFT, padx=10, pady=10)
+            CTkButton(self.f_info, text=source, command= lambda : self.load_file_from_disk(source)).pack(side=LEFT, padx=10, pady=10)
             [CTkButton(self.f_info, text=discs[x], 
-                        command= lambda x=x: self.loader_file_disc(Path(f'/media/{user}', discs[x])))
+                        command= lambda x=x: self.load_file_from_disk(Path(f'/media/{user}', discs[x])))
             .pack(side=LEFT, padx=10, pady=10) for x in range(len(discs))]
             
-    def loader_file_disc(self, path):
+    def load_file_from_disk(self, path):
         #Loads disks and files on disk
         self.current_path = path
-        self.loader_files()
+        self.upload_files()
     
-    def loader_current_path(self):
+    def load_current_path(self):
         #Loads current path and menu into information frame
         [x.destroy() for x in self.f_current_path.winfo_children()]
         CTkLabel(self.f_current_path, text=self.current_path, font=self.label_font).pack(anchor=W, side=LEFT)
+        #Alterar menu de opcoes
         self.f_current_path.menubutton_1 = Menubutton(self.f_current_path, text='...', style='TMenubutton',width=2)
         self.f_current_path.menubutton_1.menu = Menu(self.f_current_path.menubutton_1, tearoff=0, font=('Roboto Slab', 14), background='white', borderwidth=2)
         self.f_current_path.menubutton_1['menu'] = self.f_current_path.menubutton_1.menu
         self.f_current_path.menubutton_1.menu.add_checkbutton(label='Hidden Folders', onvalue='on',
-                                                              offvalue='off', variable=self.hidden_folder, command=self.loader_files)
+                                                              offvalue='off', variable=self.hidden_folder, command=self.upload_files)
         self.f_current_path.menubutton_1.pack(anchor=E)
           
-    def loader_search_widgets(self):
+    def load_search_widgets(self):
         #Loads search frame widgets
         self.search = CTkEntry(self.f_search, font=self.entry_font, height=40)
         self.search.pack(side=LEFT, fill=X, expand=True, padx=10, pady=10)
-        CTkButton(self.f_search, text='', image=self.icon_search, command=self.search_file,
+        CTkButton(self.f_search, text='', image=self.icon_search, command=self.file_search,
                   width=30, fg_color='transparent').pack(side=RIGHT, padx=10, pady=10)
-        self.search.bind('<Return>', self.search_file)
+        self.search.bind("<KeyPress>", self.file_search)
+        self.search.bind('<Return>', self.file_search)
         
-    def search_file(self, event=None):
+    def file_search(self, event=None):
         #Upload files with searched name
         file_name = self.search.get()
         current_path = self.current_path
-        print(file_name, current_path)
         self.tview_files.delete(*self.tview_files.get_children())
         self.tview_files.insert('', END, values='...', image=self.icon_return)
         for file in os.listdir(Path(current_path)):
